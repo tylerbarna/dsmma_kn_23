2001b5d1f54a775343742f8b07f9180f89fed657
Tyler Barna <barna314@umn.edu>
2023-03-12 14:32:41 -0500

    added tuned priors; updated lc_script

--- lightcurve_script.py
+++ lightcurve_script.py
@@ -5,6 +5,7 @@ import argparse
 import glob
 from pathlib import Path
 import json
+import time
 
 import numpy as np
 import pandas as pd
@@ -17,8 +18,17 @@ from astropy.time import Time
 
 ## want to generate 1 kilonova, one afterfglow, and seven supernovae
 #subprocess.run('conda activate nmma', shell=True)
+parser = argparse.ArgumentParser(description='Generate light curves for a given model')
 
-models = ['nugent-hyper','Bu2019lm','TrPi2018'] #['nugent-hyper','Bu2019lm','TrPi2018'] #
+parser.add_argument('-m','--models', type=str, nargs='+', default=['nugent-hyper','Bu2019lm','TrPi2018'], help='models to generate light curves for')
+
+args = parser.parse_args()
+models = args.models
+
+if not os.path.exists('./injections'):
+    os.mkdir('./injections')
+
+#models = ['TrPi2018']#['nugent-hyper','Bu2019lm','TrPi2018'] #
 #priors = [ os.path.join('/home/cough052/shared/NMMA/priors/',model) for model in models]
 priors = [os.path.join('./priors/',model+'.prior') for model in models]
 
@@ -56,6 +66,9 @@ def convert_lc_json(lc_file):
     '''
     takes the output file of lc_gen and converts the json into a dat file readable by nmma's light_curve_analysis
     '''
+    if not os.path.exists(lc_file):
+        print('file {} does not exist'.format(lc_file))
+        exit()
     with open(lc_file, 'r') as f:
         data = json.load(f)
     df_all = pd.DataFrame()
@@ -84,13 +97,15 @@ def lc_gen(model, inj_path, out_path,inj_label='injection',filters='r,g,i'):
                '--ztf-uncertainties',
                '--ztf-sampling',
                '--ztf-ToO', '180',
+               '--filters', filters,
                '--outdir', out_path,
                ]
-    print("lcg command: {}".format(' '.join(cmd_str)))
-    if model == 'nugent-hyper':
-        cmd_str.append('--filters')
-        cmd_str.append(filters)
+    
+    #if model == 'nugent-hyper':
+    # cmd_str.append('--filters')
+    # cmd_str.append(filters)
     command = ' '.join(cmd_str)
+    print("lcg command: {}".format(command))
     subprocess.run(command, shell=True)
 
     # cmd_str = ['light_curve_analysis',
@@ -139,7 +154,8 @@ def lc_analysis_msi(model, lc_path, out_path,inj_label='injection',filters='g'):
     command = ' '.join(cmd_str)
     subprocess.run(command, shell=True)
     
-
+inj_gen_time_dict = {model:[] for model in models}
+lc_gen_time_dict = {model:[] for model in models}
 for model, prior in zip(models,priors):
     print('starting model: ',model,' with prior: ',prior,'')
     print()
@@ -149,18 +165,33 @@ for model, prior in zip(models,priors):
         lc_count = 1
     
     for item in range(lc_count):
+        print('starting injection: ',item,'')
+        t0 = time.time()
         inj_path = injection_gen(model,inj_label=str(item))
         #inj_path = './injections/'+model+'_injection_'+str(item)+'.json'
-        print('created injection file: ',inj_path)
+        inj_time = time.time() - t0
+        inj_gen_time_dict[model] = np.append(inj_gen_time_dict[model], inj_time)
+        print('created injection file: {} ({:.2f} seconds)'.format(inj_path,inj_time))
+        
         lc_path = lc_gen(model=model, inj_path=inj_path, out_path='./injections/',inj_label=str(item),filters='g')
-        print('created lightcurve file: ',lc_path)
-        print()
+        lc_time = time.time() - inj_time - t0
+        lc_gen_time_dict[model] = np.append(lc_gen_time_dict[model], lc_time)
+        print('created lightcurve file: {} ({:.2f} seconds)'.format(lc_path, lc_time))
+        #print()
         lc_dat = lc_path.replace('.json','.dat')
-        print('lc_dat: ',lc_dat)
+        print('converted lightcurve dat: {}'.format(lc_dat))
+        
         #lc_analysis_msi(model=model, lc_path=lc_dat, #inj_label='injection_'+str(item),filters='g') ## may need to correct lc_path
         #print('submitted to msi: ',inj_path)
         ## removing logs since they aren't actually logging anything as of now
         log_files = glob.glob(os.path.join('./injections/','*.log'))
         os.remove(log_files[0])
         #exit()
+    print('finished model: ',model,' with prior: ',prior,'')
+    print('total injection time: {:.2f} seconds ({:.2f} average)'.format(np.sum(inj_gen_time_dict[model]), np.mean(inj_gen_time_dict[model])))
+    print('total lightcurve time: {:.2f} seconds ({:.2f} average)'.format(np.sum(lc_gen_time_dict[model]), np.mean(lc_gen_time_dict[model])))
+    print()
+    print()
+print('finished all models')
+#print('total execution time: {:.2f} seconds'.format(np.sum(inj_gen_time_dict.values())+np.sum(lc_gen_time_dict.values())))
 
