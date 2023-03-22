06cc5f4a6a366da283443d665594d3a9663870ed
Tyler Barna <tylerpbarna@gmail.com>
2023-03-22 15:07:30 -0500

    TrPi2018 Prior Correction
    With Michael's advice, was able to correct the TrPi2018 prior such that light curves successfully generate detectable points. generated all lightcurves again to validate.
    
    Co-Authored-By: Michael Coughlin <2291947+mcoughlin@users.noreply.github.com>

--- msi_analysis.bash
+++ msi_analysis.bash
@@ -12,4 +12,4 @@
 
 source /home/cough052/barna314/anaconda3/bin/activate nmma
 
-python /panfs/roc/groups/7/cough052/barna314/dsmma_kn_23/lightcurve_analysis.py --datafile "$1" --candname "$2" --model "$3" --prior "$4"  --nlive 128 --cpus 4
+python3 /panfs/roc/groups/7/cough052/barna314/dsmma_kn_23/lightcurve_analysis.py --datafile "$1" --candname "$2" --model "$3" --prior "$4"  --nlive 128 --cpus 4
