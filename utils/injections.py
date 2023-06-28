e47f6634b802f7f9f1fbf9690c3079d423769fb6
Tyler Barna <barna314@umn.edu>
2023-06-28 13:25:07 -0500

    Update injections.py
    added a function for retrieving parameters from an injection. Will be useful for the plotting/analysis

--- utils/injections.py
+++ utils/injections.py
@@ -1,10 +1,12 @@
 '''
 functions for generating injections. Assumes nmma is at least updated to version 0.11.1, which added zenodo support as well as corrections to several previous issues relating to injection generation behaviour (naming and inclusion of simulation_id).
 '''
-
+import bilby.core
+import json
+import numpy as np
 import os 
-from argparse import Namespace 
 
+from argparse import Namespace 
 
 from nmma.eos import create_injection
 
@@ -25,7 +27,7 @@ def generate_injection(model, outDir, injection_label=None,prior=None,):
         prior = os.path.join('./priors/',model+'.prior')
     assert os.path.exists(prior), 'prior file {} does not exist'.format(prior)
     
-    injection_path = os.path.join(outDir, 'injection_'+model+'_'+injection_label+'.json')
+    injection_path = os.path.join(outDir, 'inj_'+model+'_'+injection_label+'.json')
     
     args = Namespace( ## based on Michael's implementation in nmma unit test
         prior_file=prior,
@@ -53,5 +55,33 @@ def generate_injection(model, outDir, injection_label=None,prior=None,):
     assert os.path.exists(injection_path), 'injection file {} was not created'.format(injection_path)
     
     return injection_path
+
+def get_parameters(injection_file, injection_index=0):
+    '''
+    read the parameters from the injection file and return them as a dictionary
+    
+    Args:
+    - injection_file (str): path to injection file
+    - injection_index (int, array): index of injection to read (default=0)
+    
+    Returns:
+    - parameters (dict): dictionary of parameters
+    Note: this function assumes by default that the injection file contains a single injection. To read multiple injections in a single dictionary, pass the injection_index as a list of integers, e.g. injection_index=[0,1,2]
+    '''
+    with open(injection_file,'r') as f:
+        injection = json.load(f, object_hook=bilby.core.utils.decode_bilby_json)
+    injection_content = injection['injections']
+    
+    for key, value in injection_content.items(): ## handles conversion to numpy arrays so injection_index can be a list/array of indices
+        if isinstance(value, list):
+            injection_content[key] = np.array(value)
+    
+    parameters = {
+        key: value[injection_index] for key, value in injection_content.items()
+    }
+    
+    return parameters
+    
+    
 
     
