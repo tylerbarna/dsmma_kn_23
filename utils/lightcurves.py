d7b786309dcc0734706ae55d35a8013cffbc1f02
Tyler Barna <barna314@umn.edu>
2023-08-02 13:28:07 -0500

    updated filters

--- utils/lightcurves.py
+++ utils/lightcurves.py
@@ -64,7 +64,7 @@ def generate_lightcurve(model, injection_path, outDir=None, lightcurve_label=Non
             photometry_augmentation_N_points=10,
             photometry_augmentation_filters=','.join(filters),
             photometry_augmentation_times=None,
-            plot=True, ## maybe change this to False?
+            plot=False, ## maybe change this to False?
             joint_light_curve=False,
             with_grb_injection=False,
             absolute=False,
