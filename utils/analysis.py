3b3e01bd8ad00a2770ac4b679356861359a59291
tylerbarna <tylerpbarna@gmail.com>
2023-09-20 14:26:19 -0500

    added bestfit read-in function

--- utils/analysis.py
+++ utils/analysis.py
@@ -2,6 +2,7 @@
 functions for analysis of generated lightcurves
 '''
 
+import json
 import numpy as np
 import os
 import pandas as pd
@@ -331,3 +332,26 @@ def get_trigger_time(lightcurve_path):
     lc_keys = list(lightcurve_df.keys())
     trigger_time = lightcurve_df[lc_keys[0]][0][0]
     return trigger_time
+
+
+def read_best_fit_params(json_path):
+    '''
+    Reads in a best fit parameters json and returns a dictionary of the best fit parameters as well as a pandas dataframe of the best fit lightcurve with associated times
+    
+    Args:
+    - json_path (str): path to best fit file
+    
+    Returns:
+    - best_fit_params (dict): dictionary of best fit parameters
+    - best_fit_lightcurve (pd.DataFrame): dataframe of best fit lightcurve with associated times
+    '''
+    
+    with open(json_path) as f:
+        best_fit_params_json = json.load(f)
+        
+    ## define best fit params as best_fit_params_json with the 'Magnitudes' key removed
+    best_fit_params = best_fit_params_json.copy()
+    best_fit_lightcurve_dict = best_fit_params.pop('Magnitudes') ## this pulls double duty of both removing the 'Magnitudes' key and returning the lightcurve dict 
+    best_fit_lightcurve = pd.DataFrame.from_dict(best_fit_lightcurve_dict) ## can handle multiple filters
+    
+    return best_fit_params, best_fit_lightcurve
