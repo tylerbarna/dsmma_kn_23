c46bcd26296e386463fe2c9007eac426ebe04331
tylerbarna <tylerpbarna@gmail.com>
2023-10-30 13:59:58 -0500

    added end condition

--- generation_script.py
+++ generation_script.py
@@ -61,6 +61,7 @@ for model, prior in zip(models,priors):
                 injection_file = generate_injection(model=model, outDir=outdir, injection_label=lc_idx_zfill)
                 print('created injection file: {0}'.format(injection_file))
                 lightcurve_file = generate_lightcurve(model=model, injection_path=injection_file, outDir=outdir, filters=filters)
+                generated_lc = True
             except:
                 pass
         if validate:
