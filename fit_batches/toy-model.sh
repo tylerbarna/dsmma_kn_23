c85e8a552158b10d30fb7ab2f494a0c6d6c83353
tylerbarna <tylerpbarna@gmail.com>
2023-11-03 13:02:33 -0400

    fix batch

--- fit_batches/toy-model.sh
+++ fit_batches/toy-model.sh
@@ -14,7 +14,7 @@
 cd ~/dsmma_kn_23
 source activate nmma_dev
 
-python3 generation_script.py --outdir ~dsmma_kn_23/injections/toy-model-injections/
+python3 generation_script.py --outdir ~/dsmma_kn_23/injections/toy-model-injections/
 
 python3 analysis_script.py --data ~/dsmma_kn_23/injections/toy-model-injections/ --outdir /expanse/lustre/projects/umn131/tbarna/toy-model-fits/ --priors ~/dsmma_kn_23/priors --env nmma_dev  --cluster expanse --models nugent-hyper Me2017 TrPi2018 --tmin 3.1 --tmax 21.1 --tstep 2 --timeout 4
 
