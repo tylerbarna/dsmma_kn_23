6f02ff6ad6cad93e2003bf16cf876966d69ea7bd
tylerbarna <tylerpbarna@gmail.com>
2023-11-03 12:41:22 -0400

    housekeeping
    separated lightcurves and injections into their own folder, same for fit dataframes. also made a folder for batch scripts and wrote an example one for the toy-model using validated fits

---
+++ fit_batches/toy-model.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=shared
+#SBATCH --account=umn131
+#SBATCH --job-name=toy-model-validated
+#SBATCH --time=23:59:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8gb
+#SBATCH -o ./logs/%x_%j.out
+#SBATCH -e ./logs/%x_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ztfrest@gmail.com
+cd ~/dsmma_kn_23
+source activate nmma_dev
+
+python3 generation_script.py --outdir ~dsmma_kn_23/injections/toy-model-validated-injections/
+
+python3 analysis_script.py --data ~/dsmma_kn_23/injections/toy-model-validated-injections/ --outdir /expanse/lustre/projects/umn131/tbarna/toy-model-validated-fits/ --priors ~/dsmma_kn_23/priors --env nmma_dev  --cluster expanse --models nugent-hyper Me2017 TrPi2018 --tmin 3.1 --tmax 21.1 --tstep 2 --timeout 4
+
+echo "All done with analysis!"
+
+python3 utils/evaluation.py --lc-path ~/dsmma_kn_23/injections/toy-model-validated --fit-path /expanse/lustre/projects/umn131/tbarna/toy-model-validated-fits/ --output ./fit_dataframes/toy-model-validated-fit_df.csv
+
+echo "All done with evaluation!"
