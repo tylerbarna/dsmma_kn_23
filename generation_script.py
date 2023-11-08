99c61c2432d25f76ac1908e47ea867fd57eb91bf
tylerbarna <tylerpbarna@gmail.com>
2023-11-08 14:20:39 -0600

    newline

--- generation_script.py
+++ generation_script.py
@@ -97,7 +97,7 @@ for model, prior in zip(models,priors):
         print('generating light curve: {0}'.format(lc_idx_zfill))
         while not generated_lc:
             try:
-                print('attempt {0}'.format(retry_count))
+                print('\nattempt {0}'.format(retry_count))
                 injection_file = generate_injection(model=model, outDir=outdir, injection_label=lc_idx_zfill)
                 print('created injection file: {0}'.format(injection_file))
                 lightcurve_file = generate_lightcurve(model=model, injection_path=injection_file, outDir=outdir, filters=filters, time_series=time_series, ztf_sampling=ztf_sampling, plot=plot)
