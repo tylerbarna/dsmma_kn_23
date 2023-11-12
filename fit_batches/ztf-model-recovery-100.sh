44149b6aaa2ca90e6ce4d60a8b611ec446cb72ef
tylerbarna <tylerpbarna@gmail.com>
2023-11-12 15:07:38 -0600

    21 day analysis batch
    modified batch so it just initiates the analysis for t=21 since the others are complete

--- fit_batches/ztf-model-recovery-100.sh
+++ fit_batches/ztf-model-recovery-100.sh
@@ -14,38 +14,38 @@
 cd ~/dsmma_kn_23
 source activate nmma_dev
 
-echo "Generating injections..."
-python3 generation_script.py --models Bu2019lm nugent-hyper TrPi2018 Piro2021 --outdir ./injections/ztf-cadence-100 --min-detections 3 --min-detections-cutoff 5 --multiplier 100 --ztf-sampling 
+# echo "Generating injections..."
+# python3 generation_script.py --models Bu2019lm nugent-hyper TrPi2018 Piro2021 --outdir ./injections/ztf-cadence-100 --min-detections 3 --min-detections-cutoff 5 --multiplier 100 --ztf-sampling 
 
 
 echo "Running analysis..."
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits --priors ~/dsmma_kn_23/priors --env nmma_dev  --cluster expanse --models nugent-hyper --tmin 2 --tmax 3.1 --tstep 1 --timeout 4
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits --priors ~/dsmma_kn_23/priors --env nmma_dev  --cluster expanse --models nugent-hyper --tmin 2 --tmax 3.1 --tstep 1 --timeout 4
 
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Bu2019lm --tmin 2 --tmax 3.1 --tstep 1 --timeout 4
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Bu2019lm --tmin 2 --tmax 3.1 --tstep 1 --timeout 4
 
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models TrPi2018 --tmin 2 --tmax 3.1 --tstep 1 --timeout 4
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models TrPi2018 --tmin 2 --tmax 3.1 --tstep 1 --timeout 4
 
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Piro2021 --tmin 2 --tmax 3.1 --tstep 1 --timeout 4
-
-
-
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits --priors ~/dsmma_kn_23/priors --env nmma_dev  --cluster expanse --models nugent-hyper --tmin 5 --tmax 5.1 --tstep 1 --timeout 4
-
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Bu2019lm --tmin 5 --tmax 5.1 --tstep 1 --timeout 4
-
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models TrPi2018 --tmin 5 --tmax 5.1 --tstep 1 --timeout 4
-
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Piro2021 --tmin 5 --tmax 5.1 --tstep 1 --timeout 4
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Piro2021 --tmin 2 --tmax 3.1 --tstep 1 --timeout 4
 
 
 
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits --priors ~/dsmma_kn_23/priors --env nmma_dev  --cluster expanse --models nugent-hyper --tmin 10 --tmax 10.1 --tstep 1 --timeout 4
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits --priors ~/dsmma_kn_23/priors --env nmma_dev  --cluster expanse --models nugent-hyper --tmin 5 --tmax 5.1 --tstep 1 --timeout 4
 
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Bu2019lm --tmin 10 --tmax 10.1 --tstep 1 --timeout 4
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Bu2019lm --tmin 5 --tmax 5.1 --tstep 1 --timeout 4
 
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models TrPi2018 --tmin 10 --tmax 10.1 --tstep 1 --timeout 4
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models TrPi2018 --tmin 5 --tmax 5.1 --tstep 1 --timeout 4
 
-python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Piro2021 --tmin 10 --tmax 10.1 --tstep 1 --timeout 4
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Piro2021 --tmin 5 --tmax 5.1 --tstep 1 --timeout 4
+
+
+
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits --priors ~/dsmma_kn_23/priors --env nmma_dev  --cluster expanse --models nugent-hyper --tmin 10 --tmax 10.1 --tstep 1 --timeout 4
+
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Bu2019lm --tmin 10 --tmax 10.1 --tstep 1 --timeout 4
+
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models TrPi2018 --tmin 10 --tmax 10.1 --tstep 1 --timeout 4
+
+# python3 analysis_script.py --data ~/dsmma_kn_23/injections/ztf-cadence-100 --outdir /expanse/lustre/projects/umn131/tbarna/ztf-cadence-100-fits  --priors ~/dsmma_kn_23/priors --env nmma_dev --cluster expanse --models Piro2021 --tmin 10 --tmax 10.1 --tstep 1 --timeout 4
 
 
 
