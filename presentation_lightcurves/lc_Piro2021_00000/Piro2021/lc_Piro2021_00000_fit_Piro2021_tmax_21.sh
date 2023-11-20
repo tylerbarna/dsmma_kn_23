7e9b32afd69a58233c61ba79e8828d3ec24bb9a2
tylerbarna <tylerpbarna@gmail.com>
2023-11-19 20:53:05 -0600

    fits for presentation

---
+++ presentation_lightcurves/lc_Piro2021_00000/Piro2021/lc_Piro2021_00000_fit_Piro2021_tmax_21.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=shared
+#SBATCH --account=umn131
+#SBATCH --job-name=lc_Piro2021_00000_fit_Piro2021_tmax_21
+#SBATCH --time=01:59:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8gb
+#SBATCH -o presentation_lightcurves/lc_Piro2021_00000/Piro2021/%x_%j.out
+#SBATCH -e presentation_lightcurves/lc_Piro2021_00000/Piro2021/%x_%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ztfrest@gmail.com
+cd /expanse/lustre/projects/umn131/tbarna/
+source activate nmma_env
+lightcurve-analysis --data /home/tbarna/dsmma_kn_23/presentation_lightcurves/lc_Piro2021_00000.json --model Piro2021 --label lc_Piro2021_00000_fit_Piro2021_tmax_21 --prior ~/dsmma_kn_23/priors/Piro2021.prior --svd-path ~/dsmma_kn_23/svdmodels --filters ztfg --tmin 0.1 --tmax 21.0 --dt 0.5 --trigger-time 44243.75204109094 --error-budget 1 --nlive 1024 --ztf-uncertainties --ztf-ToO 180 --outdir presentation_lightcurves/lc_Piro2021_00000/Piro2021 --bestfit  --detection-limit "{'r':21.5, 'g':21.5, 'i':21.5}" --remove-nondetections --plot
