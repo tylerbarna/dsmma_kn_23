25dd04fb223d2608dae8679dbb72ad813112834b
tylerbarna <tylerpbarna@gmail.com>
2024-02-22 16:35:03 -0600

    handle existing folder but no json

--- utils/analysis.py
+++ utils/analysis.py
@@ -42,7 +42,8 @@ def lightcurve_analysis(lightcurve_path, model, prior, outdir, label, tmax=None,
         ## find the besfit_params json in the directory and then move it to a new directory in the root outdir
         best_fit_params = [f for f in os.listdir(outdir) if 'bestfit_params' in f]
         ## copy to the lightcurve_outdir and append the timestamp to the end of the filename to keep it unique 
-        shutil.copy(os.path.join(outdir, best_fit_params[0]), os.path.join('../',outdir, best_fit_params[0].split('.')[0] + '_' + str(int(time.time())) + '.json'))
+        if best_fit_params:
+            shutil.copy(os.path.join(outdir, best_fit_params[0]), os.path.join('../',outdir, best_fit_params[0].split('.')[0] + '_' + str(int(time.time())) + '.json'))
         shutil.rmtree(outdir)
     os.makedirs(outdir, exist_ok=True)
     env = kwargs.get('env', 'nmma_dev')
