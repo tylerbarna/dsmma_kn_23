5f54629ae4e29d231d3543aeae142e3582005098
tylerbarna <tylerpbarna@gmail.com>
2024-03-11 11:58:47 +0100

    fix merge

--- merge_fit_stats.py
+++ merge_fit_stats.py
@@ -8,10 +8,11 @@ def merge_json_files(start_dir, output_file):
         for file in files:
             if file == 'fit_stats.json':
                 file_path = os.path.join(root, file)
-                file_path = file_path.replace('/expanse/lustre/projects/umn131/tbarna/ucbeval/Me2017/ucb', '')
+                file_name = file_path.replace('/expanse/lustre/projects/umn131/tbarna/ucbeval/Me2017/ucb/', '')
+                file_name = file_name.replace('/fit_stats.json', '')
                 with open(file_path, 'r') as f:
                     data = json.load(f)
-                    merged_data[file_path] = data
+                    merged_data[file_name] = data
     merged_data = sorted(merged_data.items(), key=lambda x: x[0])
 
     with open(output_file, 'w') as f:
