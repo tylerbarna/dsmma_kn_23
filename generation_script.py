85309c876526410020afc789975eb2ec23df5b67
tylerbarna <tylerpbarna@gmail.com>
2024-03-12 10:38:23 +0100

    define lightcurve_file as none when failed

--- generation_script.py
+++ generation_script.py
@@ -101,6 +101,7 @@ for model, prior in zip(models,priors):
             print('created injection file: {0}'.format(injection_file))
             lightcurve_file = generate_lightcurve(model=model, injection_path=injection_file, outDir=outdir, filters=filters, time_series=time_series, lightcurve_label=lc_idx_zfill, ztf_sampling=ztf_sampling)
         except Exception as e:
+            lightcurve_file = None 
             print('error when generating injected lightcurve:\n',e)
             pass
         if validate:
