fe1a0c2892e33f6b40b596abb8d0c2f79c080d7f
tylerbarna <tylerpbarna@gmail.com>
2024-04-14 13:45:22 -0500

    boost cpu and memory request

--- ztfSamplingEval.sh
+++ ztfSamplingEval.sh
@@ -6,8 +6,8 @@
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=8gb
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32gb
 #SBATCH -o ./logs/%x_%j.out
 #SBATCH -e ./logs/%x_%j.err
 #SBATCH --mail-type=ALL
